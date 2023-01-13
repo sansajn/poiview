cc74e663218fd4a211702c73929891b96bbdb603
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2023-01-13 16:42:08 +0100

    map view

--- POIView/app/src/main/java/com/example/poiview/FirstFragment.kt
+++ POIView/app/src/main/java/com/example/poiview/FirstFragment.kt
@@ -1,12 +1,28 @@
 package com.example.poiview
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.DrawableRes
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.navigation.fragment.findNavController
 import com.example.poiview.databinding.FragmentFirstBinding
+import com.mapbox.geojson.Point
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
+import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
+
+val praguePos = Point.fromLngLat(14.434564115508454, 50.08353884895491)
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -26,19 +42,70 @@ class FirstFragment : Fragment() {
 
 		_binding = FragmentFirstBinding.inflate(inflater, container, false)
 		return binding.root
-
 	}
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 
-		binding.buttonFirst.setOnClickListener {
-			findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-		}
+//		binding.buttonFirst.setOnClickListener {
+//			findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//		}
+
+		mapView = view.findViewById(R.id.mapView)
+		mapView?.getMapboxMap()?.loadStyleUri(
+			Style.MAPBOX_STREETS,
+			object : Style.OnStyleLoaded {
+				override fun onStyleLoaded(style: Style) {
+					Log.d("marker", "map loaded")
+					addAnotationToMap()
+				}
+			})
+
 	}
 
 	override fun onDestroyView() {
 		super.onDestroyView()
 		_binding = null
 	}
+
+	private fun addAnotationToMap() {
+		// Create an instance of the Annotation API and get the PointAnnotationManager.
+		bitmapFromDrawableRes(requireContext(), R.drawable.red_marker)?.let {
+			val annotationApi = mapView?.annotations
+			val pointAnnotationManager = annotationApi?.createPointAnnotationManager(mapView!!)
+
+			// Set options for the resulting symbol layer.
+			val pointAnnotationOptions: PointAnnotationOptions =
+				PointAnnotationOptions()
+					.withPoint(praguePos)  // Define a geographic coordinate.
+					.withIconImage(it)  // Specify the bitmap you assigned to the point annotation
+
+			// Add the resulting pointAnnotation to the map.
+			pointAnnotationManager?.create(pointAnnotationOptions)
+		}
+	}
+
+	private fun bitmapFromDrawableRes(context: Context, @DrawableRes resourceId: Int) = convertDrawableToBitmap(
+		AppCompatResources.getDrawable(context, resourceId))
+
+	private fun convertDrawableToBitmap(sourceDrawable: Drawable?): Bitmap? {
+		if (sourceDrawable == null)
+			return null
+
+		return if (sourceDrawable is BitmapDrawable) {
+			sourceDrawable.bitmap
+		} else {
+			// copying drawable object to not manipulate on the same reference
+			val constantState = sourceDrawable.constantState ?: return null
+			val drawable = constantState.newDrawable().mutate()
+			val bitmap: Bitmap = Bitmap.createBitmap(
+				drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+			val canvas = Canvas(bitmap)
+			drawable.setBounds(0, 0, canvas.width, canvas.height)
+			drawable.draw(canvas)
+			bitmap
+		}
+	}
+
+	private var mapView: MapView? = null
 }
