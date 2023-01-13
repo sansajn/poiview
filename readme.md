4dec9feb397fa003143026b661fdbeccfd82458e
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2023-01-13 17:11:52 +0100

    readme and script update

--- readme.md
+++ readme.md
@@ -1,1 +1,23 @@
-POI View repository ...
+# POI view
+
+To get POIView app running configure secret token first.
+
+- Open (or create) `~/.gradle/gradle.properties`
+- define `MAPBOX_DOWNLOADS_TOKEN` variable with access token this way
+
+```
+MAPBOX_DOWNLOADS_TOKEN=YOUR_SECRET_MAPBOX_ACCESS_TOKEN
+```
+
+> for access token, create Mapbox account, then go to *Tokens* section and create token with `Downloads:Read` checked in a *Secret* section.
+
+Then configure your public token.
+
+> **note**: this is already done
+
+- create dedicated public token for sample in Mapbox account
+- define `mapbox_access_token` in `res/values/strings.xml` file this way
+
+```xml
+<string name="mapbox_access_token">pk.eyJ1IjoiYWRhbS1obGF2YXRvdmljIiwiYSI6ImNsY291Z3IwMzAyM2MzcHF5dmZ3b3J3OW8ifQ.Dn3oDvBJKVHgOKYyqaZyVA</string>
+```
--- script/create_db.sql
+++ script/create_db.sql
@@ -1,12 +1,12 @@
 CREATE TABLE poi (id INTEGER PRIMARY KEY, lon REAL, lat REAL, name TEXT);
 
-INSERT INTO poi (lon, lat, name) VALUES(50.1044106, 14.4499106, 'Holesovice');
-insert into poi (lon, lat, name) values(50.0964894, 14.4539178, 'Karlin');
-insert into poi (lon, lat, name) values(50.0847706, 14.4644267, 'Zizkov');
-insert into poi (lon, lat, name) values(50.0748011, 14.4530325, 'Vinohrady');
-insert into poi (lon, lat, name) values(50.1175831, 14.4260389, 'Troja');
-insert into poi (lon, lat, name) values(50.1255628, 14.4452650, 'Kobylisy');
-insert into poi (lon, lat, name) values(50.0833386, 14.4180567, 'Stare Mesto');
-insert into poi (lon, lat, name) values(50.0788497, 14.4254381, 'Nove Mesto');
-insert into poi (lon, lat, name) values(50.0636178, 14.4188719, 'Vysehrad');
-insert into poi (lon, lat, name) values(50.0843850, 14.4028644, 'Mala Strana');
+INSERT INTO poi (lon, lat, name) VALUES(14.4499106, 50.1044106, 'Holesovice');
+INSERT INTO poi (lon, lat, name) VALUES(14.4539178, 50.0964894, 'Karlin');
+INSERT INTO poi (lon, lat, name) VALUES(14.4644267, 50.0847706, 'Zizkov');
+INSERT INTO poi (lon, lat, name) VALUES(14.4530325, 50.0748011, 'Vinohrady');
+INSERT INTO poi (lon, lat, name) VALUES(14.4260389, 50.1175831, 'Troja');
+INSERT INTO poi (lon, lat, name) VALUES(14.4452650, 50.1255628, 'Kobylisy');
+INSERT INTO poi (lon, lat, name) VALUES(14.4180567, 50.0833386, 'Stare Mesto');
+INSERT INTO poi (lon, lat, name) VALUES(14.4254381, 50.0788497, 'Nove Mesto');
+INSERT INTO poi (lon, lat, name) VALUES(14.4188719, 50.0636178, 'Vysehrad');
+INSERT INTO poi (lon, lat, name) VALUES(14.4028644, 50.0843850, 'Mala Strana');
