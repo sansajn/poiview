96cba6fe19ecee1787c56fbf5149ae8ed21ba977
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2023-02-10 16:38:06 +0100

    gallery table in test DB

--- script/create_db.sql
+++ script/create_db.sql
@@ -10,3 +10,16 @@ INSERT INTO poi (lon, lat, name) VALUES(14.4180567, 50.0833386, 'Stare Mesto');
 INSERT INTO poi (lon, lat, name) VALUES(14.4254381, 50.0788497, 'Nove Mesto');
 INSERT INTO poi (lon, lat, name) VALUES(14.4188719, 50.0636178, 'Vysehrad');
 INSERT INTO poi (lon, lat, name) VALUES(14.4028644, 50.0843850, 'Mala Strana');
+
+CREATE TABLE gallery (id INTEGER PRIMARY KEY, lon REAL, lat REAL, date INTEGER, path TEXT);
+
+INSERT INTO gallery (lon, lat, date, path) VALUES(15.0505333, 50.7888681, 1676039073, '/test/photo1.jpg');
+INSERT INTO gallery (lon, lat, date, path) VALUES(15.0505714, 50.7823372, 1676035473, '/test/photo2.jpg');
+INSERT INTO gallery (lon, lat, date, path) VALUES(15.0471172, 50.7726478, 1676031873, '/test/photo3.jpg');
+INSERT INTO gallery (lon, lat, date, path) VALUES(15.0692225, 50.7652283, 1676028273, '/test/photo4.jpg');
+INSERT INTO gallery (lon, lat, date, path) VALUES(15.0813903, 50.7431717, 1676024673, '/test/photo5.jpg');
+INSERT INTO gallery (lon, lat, date, path) VALUES(15.0546950, 50.7302550, 1676021073, '/test/photo6.jpg');
+INSERT INTO gallery (lon, lat, date, path) VALUES(15.0552931, 50.7191353, 1676017473, '/test/photo7.jpg');
+INSERT INTO gallery (lon, lat, date, path) VALUES(15.0761556, 50.7026367, 1676013873, '/test/photo8.jpg');
+INSERT INTO gallery (lon, lat, date, path) VALUES(15.1169319, 50.6991692, 1676010273, '/test/photo9.jpg');
+INSERT INTO gallery (lon, lat, date, path) VALUES(15.1413058, 50.7085036, 1676006673, '/test/photo10.jpg');
--- script/readme.md
+++ script/readme.md
@@ -1,7 +1,7 @@
 run
 
 ```bash
-sqlite3 test.db < create_db.sqt
+sqlite3 test.db < create_db.sql
 ```
 
 to create test database `test.db`
