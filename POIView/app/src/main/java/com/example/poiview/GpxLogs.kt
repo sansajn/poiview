02f5662d240efaa530cd49d0d4f80437fdb5fa73
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2023-11-06 17:42:12 +0100

    trip_logs directory omved from sd card to internal memory

--- POIView/app/src/main/java/com/example/poiview/GpxLogs.kt
+++ POIView/app/src/main/java/com/example/poiview/GpxLogs.kt
@@ -1,5 +1,6 @@
 package com.example.poiview
 
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,36 +8,25 @@ import java.io.File
 import kotlin.system.measureTimeMillis
 
 /** Helps to handle GPX log trips.
- * SD card access permission required. */
+ * External volume access permission required. */
 class GpxLogs(private val parentFragment: Fragment) {
-	// TODO: this will not work on devices without SD Card!! Maybe I should move trip_logs to internal storage.
-	/** @returns list of GPX files in `trip_logs` directory (e.g. `/storage/9C33-6BBD/trip_logs`). */
+	/** @returns list of GPX files in `trip_logs` directory (e.g. `/storage/emulated/0/trip_logs`). */
 	fun list(): ArrayList<String> {
-		// TODO: move code to the module and reus the module there and in GalleryPhotoBatch to access SDCard folders
-		// figure out trip_logs path e.g. /storage/9C33-6BBD/trip_logs
-
-		val externalVolumes = MediaStore.getExternalVolumeNames(parentFragment.requireContext())
-		val sdCardVolumes = externalVolumes.filter {
-			it != MediaStore.VOLUME_EXTERNAL_PRIMARY && it != MediaStore.VOLUME_EXTERNAL
-		}
-		if (sdCardVolumes.isEmpty())  // no SD Card
-			return arrayListOf<String>()
-
-		// TODO: for now just take the first one SD Card in system
-		val sdCardTripLogPath = "/storage/${sdCardVolumes[0].uppercase()}/trip_logs"
-		Log.d(MapFragment.TAG, "sdCardTripLogPath=$sdCardTripLogPath")
+		// figure out trip_logs path e.g. /storage/emulated/0/trip_logs
+		val externalStorageDir = Environment.getExternalStorageDirectory()
+		val tripLogPath = "$externalStorageDir/trip_logs"
+		Log.d(TAG, "tripLogPath=$tripLogPath")
 
 		val result = arrayListOf<String>()
 
 		val elapsed = measureTimeMillis {
-			// TODO: this seems to be work for map, not forEach
-			File(sdCardTripLogPath).walk().forEach {
+			File(tripLogPath).walk().forEach {
 				if (it.isFile && it.extension == "gpx")
 					result.add(it.absolutePath)
 			}
 		}
 
-		Log.d(TAG, "listing SD Card trip_logs folder ($sdCardTripLogPath) (${result.size}): ${elapsed}ms")
+		Log.d(TAG, "listing trip_logs folder ($tripLogPath) (${result.size}): ${elapsed}ms")
 
 		return result
 	}
